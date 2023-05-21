e42b1d4c050a2569db3ad708e2f95e9a65141f1b
tom <tom@that-place>
2023-05-21 01:25:36 -0500

    intial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject functionaldbdemo "0.1.0-SNAPSHOT"
+  :description "transcription of Yoav Rubin's database from the online book '500 Lines or Less: An Archaeology-Inspired Database', modified to work with modern clojure"
+  :url "https://aosabook.org/en/500L/an-archaeology-inspired-database.html"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.11.1"]])
